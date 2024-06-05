76d144d1096ed2b20ff6c401a69afc5ac726d143
MorrisNijland3 <33375505+MorrisNijland3@users.noreply.github.com>
2024-06-05 19:37:07 +0200

    Update test.py

--- backend/test.py
+++ backend/test.py
@@ -4,6 +4,9 @@ import requests
 app = Flask(__name__)
 
 @app.route('/api', methods=['GET'])
+def index():
+    return index.html
+
 def api():
     authentication = 'https://api.sportmonks.com/api/v3/football/livescores?api_token=vxFaxsQl6QnaXOBhmCG4dbPiWGfg6wykzKbZBqYGOn28ZZwvZ7pw8E5jtTxq'
     endpoint = "https://api.sportmonks.com/v3/football"  
