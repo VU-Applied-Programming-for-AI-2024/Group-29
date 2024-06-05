21d7fdcefb27e44183b85f98a39dc050c885a448
MorrisNijland3 <morrisnijland@gmail.com>
2024-06-05 12:33:38 +0200

    Update test.py

--- backend/test.py
+++ backend/test.py
@@ -1,12 +1,13 @@
 from flask import Flask, request, jsonify
+import requests
 
 app = Flask(__name__)
 
 @app.route('/api', methods=['GET'])
 def api():
-    url = ""  # Add your URL here
-
-    response = request.get(url)
+    authentication = 'https://api.sportmonks.com/api/v3/football/livescores?api_token=vxFaxsQl6QnaXOBhmCG4dbPiWGfg6wykzKbZBqYGOn28ZZwvZ7pw8E5jtTxq'
+    endpoint = "https://api.sportmonks.com/v3/football"  
+    response = requests.get(authentication)
 
     if response.status_code == 200:
         data = response.json()
