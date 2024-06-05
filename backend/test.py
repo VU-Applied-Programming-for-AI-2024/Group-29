23440c67fe289a2af8aab6b51398739bfb6e32a7
MorrisNijland3 <morrisnijland@gmail.com>
2024-06-05 11:43:02 +0200

    Update test.py

--- backend/test.py
+++ backend/test.py
@@ -1,16 +1,13 @@
-import requests
-
-url = "https://api.sportsdata.io/v3/tennis/scores/json/Competitions?key=df78e801653e440996f5f16e6a9bbeb2"
+from flask import Flask, request, jsonify
+url = ""
 
 response = requests.get(url)
 
 if response.status_code == 200:
     data = response.json()
-    # Process the data returned by the API
-    # ...
     
-for each in data:
-    print('competition', each['Name'])
+    
+    print(data)
 
 else:
     print("Error:", response.status_code)
