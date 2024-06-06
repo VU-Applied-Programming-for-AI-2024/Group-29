43f5c710c897f828bdb99528e92d7dd32e1f8e77
MorrisNijland3 <morrisnijland@gmail.com>
2024-06-06 21:22:57 +0200

    Update run.py

--- run.py
+++ run.py
@@ -1,4 +1,14 @@
-from backend import app
+from flask import Flask, send_from_directory
+
+app = Flask(__name__, static_folder='frontend', static_url_path='')
+
+@app.route('/')
+def serve_index():
+    return send_from_directory(app.static_folder, 'index.html')
+
+@app.route('/<path:path>')
+def serve_static(path):
+    return send_from_directory(app.static_folder, path)
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
