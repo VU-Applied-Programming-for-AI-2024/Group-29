7de081ac2e7e1c6080323b3d7e954e6e50d3fb5d
MorrisNijland3 <morrisnijland@gmail.com>
2024-06-06 21:28:11 +0200

    Update run.py

--- run.py
+++ run.py
@@ -1,6 +1,9 @@
 from flask import Flask, send_from_directory
+from backend.routes import backend
 
 app = Flask(__name__, static_folder='frontend', static_url_path='')
+
+app.register_blueprint(backend)
 
 @app.route('/')
 def serve_index():
