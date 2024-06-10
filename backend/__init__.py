cea121ce629f2b327993b54fccecb6dbad187f81
MorrisNijland3 <morrisnijland@gmail.com>
2024-06-10 09:09:01 +0200

    Update __init__.py

--- backend/__init__.py
+++ backend/__init__.py
@@ -1,5 +1,5 @@
 from flask import Flask, request, jsonify
-from backend.routes import backend  
+from backend.routes import backend
 
 app = Flask(__name__)
 
@@ -8,7 +8,6 @@ app.register_blueprint(backend)
 @app.route('/api/submit', methods=['POST'])
 def submit_form():
     data = request.json
-
     print("Form Data:", data)
     return jsonify({'message': 'Form submitted successfully'})
 
