f0455766daa84929eba6d6efb545b4fe28e97526
MorrisNijland3 <morrisnijland@gmail.com>
2024-06-10 09:09:11 +0200

    Update routes.py

--- backend/routes.py
+++ backend/routes.py
@@ -1,14 +1,19 @@
-from flask import Blueprint
+from flask import Blueprint, jsonify
 import http.client
+import json
 
 backend = Blueprint('backend', __name__)
 
-@backend.route('/api', methods=['GET'])
+@backend.route('/api/teams', methods=['GET'])
 def fetch_teams():
     conn = http.client.HTTPSConnection("api.football-data.org")
     payload = ''
-    headers = {}
+    headers = {
+        'X-Auth-Token': 'e01ad631ff844c349b745cb14b38e770'
+    }
     conn.request("GET", "/v4/competitions", payload, headers)
     res = conn.getresponse()
     data = res.read()
-    return data.decode("utf-8")
+    data_json = json.loads(data.decode("utf-8"))
+
+    return jsonify(data_json)
